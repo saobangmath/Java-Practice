737173f208e46cae7c8d273dd1b2bc58c552ee57
saobangmath <anhtai2672000@gmail.com>
2020-06-09 16:12:40 +0800

    no more love

--- src/ImageUtil/Constant.java
+++ src/ImageUtil/Constant.java
@@ -3,5 +3,5 @@ package ImageUtil;
 public class Constant {
     protected static int x = 20;
     protected static int y = 20;
-    protected static String secret = "I love Thu Minh!";
+    protected static String secret = "I will be more mature day by day!";
 }
